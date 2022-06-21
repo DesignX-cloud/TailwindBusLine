f29c467b1b22b13f5da2cae9fe273a10f04f5aec
Harmonia <designyunyun@gmail.com>
2022-06-22 04:00:34 +0800

    '新增header/footer/banner-omponent'

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
 
   theme: {
-    extend: {},
+    extend: {
+      inset: {
+        '1/6': '15%',
+      }
+    },
     screens: {
       '2xl': { 'max': '1439px' },
       // => @media (max-width: 1439px) { ... }
@@ -25,12 +29,23 @@ module.exports = {
 
       'sm': { 'max': '575px' },
       // => @media (max-width: 575px) { ... }
-      
+
       'xs': { 'max': '474px' },
       // => @media (max-width: 474px) { ... }
 
       'ss': { 'max': '374px' },
       // => @media (max-width: 374px) { ... }
+    },
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '.1em',
+      widest: '.25em',
+      x: '1em',
     }
   },
   plugins: [],
